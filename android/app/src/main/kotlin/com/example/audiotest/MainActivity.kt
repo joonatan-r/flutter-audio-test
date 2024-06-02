9ffdfbf3aa974f14cab9f84759c19c641fd13ace
joonatan-r <joonatan.rantanen@gmail.com>
2024-06-02 20:29:22 +0300

    show closest note and difference

--- android/app/src/main/kotlin/com/example/audiotest/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/audiotest/MainActivity.kt
@@ -19,6 +19,7 @@ import java.nio.ByteOrder
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.math.cos
+import kotlin.math.abs
 
 class MainActivity : FlutterActivity() {
 
@@ -127,7 +128,7 @@ class MainActivity : FlutterActivity() {
             return if (!freq.isFinite() || freq > MAX_HZ_TO_DISPLAY) {
                     "-"
                 } else {
-                    String.format("%.2f", freq)
+                    "${String.format("%.2f", freq)} (${Freqs.getClosest(freq)})"
                 }
         }
 
@@ -249,5 +250,118 @@ class MainActivity : FlutterActivity() {
                 return arrayOf(binNo, binNo2, binNo3)
             }
         }
+    }
+}
+
+object Freqs {
+    val asMap =
+        mapOf(
+            "B7"                 to 3951.066,
+            "A#7/Bb7"            to 3729.310,
+            "A7"                 to 3520.000,
+            "G#7/Ab7"            to 3322.438,
+            "G7"                 to 3135.963,
+            "F#7/Gb7"            to 2959.955,
+            "F7"                 to 2793.826,
+            "E7"                 to 2637.020,
+            "D#7/Eb7"            to 2489.016,
+            "D7"                 to 2349.318,
+            "C#7/Db7"            to 2217.461,
+            "C7"                 to 2093.005,
+            "B6"                 to 1975.533,
+            "A#6/Bb6"            to 1864.655,
+            "A6"                 to 1760.000,
+            "G#6/Ab6"            to 1661.219,
+            "G6"                 to 1567.982,
+            "F#6/Gb6"            to 1479.978,
+            "F6"                 to 1396.913,
+            "E6"                 to 1318.510,
+            "D#6/Eb6"            to 1244.508,
+            "D6"                 to 1174.659,
+            "C#6/Db6"            to 1108.731,
+            "C6"                 to 1046.502,
+            "B5"                 to 987.7666,
+            "A#5/Bb5"            to 932.3275,
+            "A5"                 to 880.0000,
+            "G#5/Ab5"            to 830.6094,
+            "G5"                 to 783.9909,
+            "F#5/Gb5"            to 739.9888,
+            "F5"                 to 698.4565,
+            "E5"                 to 659.2551,
+            "D#5/Eb5"            to 622.2540,
+            "D5"                 to 587.3295,
+            "C#5/Db5"            to 554.3653,
+            "C5"                 to 523.2511,
+            "B4"                 to 493.8833,
+            "A#4/Bb4"            to 466.1638,
+            "A4 A440"            to 440.0000,
+            "G#4/Ab4"            to 415.3047,
+            "G4"                 to 391.9954,
+            "F#4/Gb4"            to 369.9944,
+            "F4"                 to 349.2282,
+            "E4"                 to 329.6276,
+            "D#4/Eb4"            to 311.1270,
+            "D4"                 to 293.6648,
+            "C#4/Db4"            to 277.1826,
+            "C4"                 to 261.6256,
+            "B3"                 to 246.9417,
+            "A#3/Bb3"            to 233.0819,
+            "A3"                 to 220.0000,
+            "G#3/Ab3"            to 207.6523,
+            "G3"                 to 195.9977,
+            "F#3/Gb3"            to 184.9972,
+            "F3"                 to 174.6141,
+            "E3"                 to 164.8138,
+            "D#3/Eb3"            to 155.5635,
+            "D3"                 to 146.8324,
+            "C#3/Db3"            to 138.5913,
+            "C3"                 to 130.8128,
+            "B2"                 to 123.4708,
+            "A#2/Bb2"            to 116.5409,
+            "A2"                 to 110.0000,
+            "G#2/Ab2"            to 103.8262,
+            "G2"                 to 97.99886,
+            "F#2/Gb2"            to 92.49861,
+            "F2"                 to 87.30706,
+            "E2"                 to 82.40689,
+            "D#2/Eb2"            to 77.78175,
+            "D2"                 to 73.41619,
+            "C#2/Db2"            to 69.29566,
+            "C2"                 to 65.40639,
+            "B1"                 to 61.73541,
+            "A#1/Bb1"            to 58.27047,
+            "A1"                 to 55.00000,
+            "G#1/Ab1"            to 51.91309,
+            "G1"                 to 48.99943,
+            "F#1/Gb1"            to 46.24930,
+            "F1"                 to 43.65353,
+            "E1"                 to 41.20344,
+            "D#1/Eb1"            to 38.89087,
+            "D1"                 to 36.70810,
+            "C#1/Db1"            to 34.64783,
+            "C1"                 to 32.70320,
+            "B0"                 to 30.86771,
+            "A#0/Bb0"            to 29.13524,
+            "A0"                 to 27.50000,
+            "G#0/Ab0"            to 25.95654,
+            "G0"                 to 24.49971,
+            "F#0/Gb0"            to 23.12465,
+            "F0"                 to 21.82676,
+            "E0"                 to 20.60172,
+            "D#0/Eb0"            to 19.44544,
+            "D0"                 to 18.35405,
+            "C#0/Db0"            to 17.32391,
+            "C0"                 to 16.35160,
+        )
+
+    fun getClosest(freq: Double): String {
+        val closest = asMap.entries.minByOrNull { abs(it.value - freq) }
+        if (closest == null) {
+            return ""
+        }
+        val noteWithoutOctave = closest.key.filter { !it.isDigit() }
+        val diffSign = if (freq > closest.value) "+" else "-"
+        val diff = String.format("%.2f", abs(freq - closest.value))
+        return "${noteWithoutOctave} ${diffSign} ${diff}"
     }
 }
